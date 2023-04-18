dabdf99220e0b538c18bbb31d41151f0b95cbed6
Diovane Alves <alvdiovane@gmail.com>
2023-04-18 19:53:22 -0300

    Configuring TailwindCSS and adding project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#121214",
+        text: "#FFF",
+      },
+    },
   },
   plugins: [],
-}
-
+};
